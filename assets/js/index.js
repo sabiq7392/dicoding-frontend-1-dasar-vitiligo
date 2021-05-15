4a6970798c39cd8675f8dc3f1be9f5ab0d2e396b
Sabiq Muhammad <72848275+sabiq7392@users.noreply.github.com>
2021-05-15 11:29:18 +0700

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,125 @@
-const humburger = document.querySelector(".humburger");
+
 const nav = document.querySelector("#nav-link-wrapper");
 const cross = document.querySelector(".crossNav");
 
+// ====================== Humburger.
+const humburger = document.querySelector(".humburger");
+const lineHum = [
+    document.querySelector("#hum-satu"),
+    document.querySelector("#hum-dua"),
+    document.querySelector("#hum-tiga")
+]
+
 humburger.addEventListener("click", function() {
-    nav.style.animation ="showNav 1000ms";
-    nav.style.display = "flex";
+    lineHum[0].classList.toggle("invert-rotated");
+    lineHum[1].classList.toggle("d-none");
+    lineHum[2].classList.toggle("rotated");
+    humburger.classList.toggle("humUp");
+    nav.classList.toggle("d-flex");
+    nav.classList.toggle("showNav");
 });
 
-cross.addEventListener("click", function() {
-    nav.style.animation = "hideNav 1000ms";
-    nav.addEventListener("transitionend", function(e) { 
-        nav.style.display = "none";
-    })
+
+// ====================== Slider
+
+const buttonSlide = [
+    document.querySelector("#firstSlide"), 
+    document.querySelector("#secondSlide"),
+    document.querySelector("#thirdSlide")
+]
+
+const heroImage = [
+    document.querySelector("#firstImage"),
+    document.querySelector("#secondImage"),
+    document.querySelector("#thirdImage")
+]
+
+if (buttonSlide[0].checked) {
+    heroImage[0].classList.add("d-flex");
+    heroImage[1].style.display = "none";
+    heroImage[2].style.display = "none";
+
+}
+
+if (buttonSlide[1].checked) {
+    heroImage[1].classList.add("d-flex");
+    heroImage[0].style.display = "none";
+    heroImage[2].style.display = "none";
+}
+
+if (buttonSlide[2].checked) {
+    heroImage[2].classList.add("d-flex");
+    heroImage[0].style.display = "none";
+    heroImage[1].style.display = "none";
+}
+
+buttonSlide[0].addEventListener("click", function(){
+    heroImage[0].classList.add("d-flex");
+    heroImage[0].classList.remove("d-none");
+
+    heroImage[1].classList.remove("d-flex");
+    heroImage[1].classList.add("d-none");
+
+    heroImage[2].classList.remove("d-flex");
+    heroImage[2].classList.add("d-none");
 });
 
-// const mediaQuariesXL = window.matchMedia("(min-width: 992px)");
-// const mediaQuariesLG = window.matchMedia("(max-width: 991px)");
+buttonSlide[1].addEventListener("click", function(){
+    heroImage[1].classList.add("d-flex");
+    heroImage[1].classList.remove("d-none");
+
+    heroImage[0].classList.remove("d-flex");
+    heroImage[0].classList.add("d-none");
+
+    heroImage[2].classList.remove("d-flex");
+    heroImage[2].classList.add("d-none");
+});
+
+buttonSlide[2].addEventListener("click", function(){
+    heroImage[2].classList.add("d-flex");
+    heroImage[2].classList.remove("d-none");
+
+    heroImage[0].classList.remove("d-flex");
+    heroImage[0].classList.add("d-none");
+
+    heroImage[1].classList.remove("d-flex");
+    heroImage[1].classList.add("d-none");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // for (let manyClick = 0; manyClick <= 2; manyClick++) {
+    //     if (manyClick == 1) {
+    //         nav.classList.add("d-flex");
+    //         manyClick--;
+    //         console.log(manyClick)
+    //     }
+    // }
+
+// cross.addEventListener("click", function() {
+//     if (nav.classList.contains("showNav") && nav.classList.contains("d-flex")) {
+//         nav.classList.remove("showNav");
+//         nav.classList.add("hideNav");
+//     }
+    // nav.addEventListener("transitionend", function(e) { 
+    //     if (nav.classList.contains("d-flex")) {
+    //         nav.classList.remove("d-flex");
+    //         nav.classList.add("d-none");
+    //     }
+    // })
+// });
+
 
 // if (mediaQuariesXL.matches) {
 //     nav.style.display = "flex";
